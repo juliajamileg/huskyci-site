603cd03de16277b068afe3b688440087f1fbccc1
juliajamileg <140719963+juliajamileg@users.noreply.github.com>
2024-02-26 14:35:12 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Husky CI',
   tagline: 'Husky CI makes it easy to find vulnerabilities inside your CI',
-  url: 'https://github.com/globocom/huskyCI',
+  // url: 'https://github.com/globocom/huskyCI',
   baseUrl: '/',
   favicon: 'img/favicon2.ico',
   organizationName: 'globocom',
